fff2e4064011b1c074acaf06e670ae77dc847fba
eoroose <eric.roose15@gmail.com>
2021-04-18 03:57:52 +0200

    header & login  styling

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,23 +1,22 @@
-<link href="./assets/css/login.css" rel="stylesheet" type="text/css" />
 
- <div class="container">
+<div class="container login-page">
 
-    <object class="logo" data="<?php echo base_url('./assets/images/login_logo.svg'); ?>"></object>
-
+    <object class="login-logo" data="<?php echo base_url('./assets/images/login_logo.svg'); ?>"></object>
+    
     <div class="login-container">
         
         <form class="" action="/" method="post">
 
             <div class="form-group pt-3 ">
-                <input type="text" class="form-control" name="username" id="username" value="<?= set_value('username')?>" placeholder="username">
+                <input type="text" class="form-control login-input" name="username" id="username" value="<?= set_value('username')?>" placeholder="username">
             </div>
 
             <div class="form-group pt-3 ">
-                <input type="password" class="form-control" name="password" id="password" value="" placeholder="password">
+                <input type="password" class="form-control login-input" name="password" id="password" value="" placeholder="password">
             </div>
 
             <?php if (isset($validation)): ?>
-                <div class="alert-message">
+                <div class="login-alert-message">
                     <div class="alert alert-danger" role="alert">
                             <?= $validation->listErrors() ?>
                     </div>
