068093444428878b6493a7bea9ed4943a871d000
eoroose <eric.roose15@gmail.com>
2021-05-03 21:55:36 +0200

    new pages messages & new sql file

--- app/Views/doctors.php
+++ app/Views/doctors.php
@@ -1,4 +1,7 @@
 <div>
     <h4>doctors page</h4>
     <h4>Check DoctorsController.php</h4>
+
+    <p>page where there will be a list of doctors, and their appointments</p>
+    <p>possibility to add new doctors</p>
 </div>
