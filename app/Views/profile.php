fdd78476185c73e4cb512f2c71f1c62855288a3a
eoroose <eric.roose15@gmail.com>
2021-05-03 03:29:29 +0200

    profile page completed

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -22,35 +22,40 @@
         <?= session()->get('succes')?>
     </div>
 <?php endif; ?>
+
 <div>
     <link href="/assets/css/profile.css" rel="stylesheet" type="text/css" />
 
-    <div class="container profile-container">
-        <div class="row profile-row main-bottom-padding">
+    <div class="container profile-container main-bottom-padding">
+        <div class="row profile-row card main-card">
             
-            <div class="col-12 card profile-col">
-                <h1 class="main-title profile-title"><?php echo $user['firstname']?> <?php echo $user['lastname']?> </h1>
-            </div>
-            <div class="col-12 card profile-col">
-                <div class="card-body dashboard-card-body">
-                    <img src="<?php echo $avatar['location'];?>" id='avatarImg' height="75px" width="75px">
-                    <h5 class="card-title">Change Avatar</h5>
-                    <a onclick="chooseAvatar()" class="stretched-link"></a>
-                </div>
-            </div>
-            <div class="col-12 card username-container">
-                
-                    <h1 class="main-title profile-title">username:</h1>
-                    <h1 class="profile-title"><?php echo $user['username']?></h1>
-                
+            <!-- AVATAR -->
+            <div class="col-12 card profile-avatar-col">
+                <img class="profile-avatar" src="<?php echo $avatar['location'];?>" id='avatarImg'>
+                <a onclick="chooseAvatar()" class="stretched-link"></a>
             </div>
 
-            <div class="col-12 card username-container">
-                <h1 class="main-title profile-title">birthday:</h1>
-                <h1 class="profile-title"><?php echo $user['birthday']?></h1>
+            <!-- FIRST AND LAST NAME -->
+            <div class="col-12 card profile-col profile-name-col">
+                <h1><?php echo $user['firstname']?> <?php echo $user['lastname']?> </h1>
             </div>
-            <div class="col-12 card">
-                <button type="button" class="main-btn" onclick="showCangePasword()" id="changeP">change password</button>
+        </div>
+
+        
+        <div class="row profile-row card main-card">
+            <!-- USERNAME -->
+            <div class="col-12 card profile-col">
+                <h1><b>username: </b> <?php echo $user['username']?> </h1>
+            </div>
+
+            <!-- BIRTHDAY -->
+            <div class="col-12 card profile-col">
+                <h1><b>birthday: </b> <?php echo $user['birthday']?> </h1>
+            </div>
+
+            <!-- CHANGE PASSWORD -->
+            <div class="col-12 card profile-col">
+                <button type="button" class="main-btn profile-password-btn" onclick="showCangePasword()" id="changeP">change password</button>
                 <?php if (isset($validation)): ?>
                     <div class="main-alert-message">
                         <div class="alert alert-danger" role="alert">
@@ -58,63 +63,81 @@
                         </div>
                     </div>
                 <?php endif; ?>
+
                 <div id="otherFieldDiv" style="display: none">
                     <form class="" action="/changePassword" method="post">
                         <input type="password" class="form-control main-input profile-input" name="old-password" id="old-password" value="" placeholder="old password">
                         <input type="password" class="form-control main-input profile-input" name="new-password" id="new-password" value="" placeholder="new password">
                         <input type="password" class="form-control main-input profile-input" name="confirm-password" id="confirm-password" value="" placeholder="confirm password">
-                        <button type="submit" class="main-btn">change password</button>
+                        <button type="submit" class="main-btn profile-password-btn">change password</button>
                     </form>
-                    </div>
                 </div>
-            <?php if(session()->get('role')=='inhabitant'){?>
-            <div class="col-12 card username-container">
-                <h1 class="main-title profile-title">Godparent:</h1>
-                <h1 class="profile-title"><?php echo $godparent['firstname'].' '.$godparent['lastname']?></h1>
             </div>
-            <?php if($godchilds==null){}else{?>
-            <div class="col-12 card username-container">
-                <h1 class="main-title profile-title">Godchilds:</h1>
-                <p class="">
-                <?php foreach ($godchilds as $childs) { ?>
-                <?php echo $childs['firstname'].' '.$childs['lastname']?><br>
-                <?php } ?>
-                </p>
-            </div>
-            <?php } ?>
-            <div class="col-12 card username-container">
-                <h1 class="main-title profile-title">appointments :</h1>
-                <p class="">
-                    <?php foreach ($appointments as $appoints) { ?>
 
-                        <?php echo 'doctor: '.$appoints['doctorFirstname'].' '.$appoints['doctorLasttname'];?> <br>
-                    <?php echo 'date:'.$appoints['dateAppointment'].' reden:'.$appoints['reason'] ;?><br>
-
-                    <?php } ?>
-                </p>
-
-            </div>
-            <?php } ?>
         </div>
+
+        <?php if(session()->get('role')=='inhabitant') {?>
+            <div class="row profile-row card main-card">
+                <!-- GODPARENT -->
+                <div class="col-12 card profile-col">
+                    <h1><b>godparent: </b> <?php echo $godparent['firstname'].' '.$godparent['lastname']?> </h1>
+                </div>
+
+                <!-- GODCHILDREN -->
+                <div class="col-12 card profile-col profile-col-godchildren-title">
+                    <h1><b>godchildren:</b></h1>
+                </div>
+
+                <?php if($godchilds==null) { ?>
+                    <div class="col-12 card profile-col profile-col-godchildren">
+                        <h1>none</h1>
+                    </div>
+                <?php } else { foreach ($godchilds as $childs) { ?>
+                    <div class="col-12 card profile-col profile-col-godchildren">
+                        <h1><?php echo $childs['firstname'].' '.$childs['lastname']?></h1>
+                    </div>
+                <?php }} ?>
+            </div>
+
+            <div class="row profile-row card main-card">
+                <!-- APPOINTMENTS -->
+                <div class="col-12 card profile-col profile-col-appointment-title">
+                    <h1><b>appointments:</b></h1>
+                </div>
+
+                <?php foreach ($appointments as $appoints) { ?>
+                    <div class="col-12 card profile-col-appointment">
+                        <h1><b>date: </b> <?php echo $appoints['dateAppointment'] ?></h1>    
+                        <h1><b>doctor: </b> <?php echo $appoints['doctorFirstname'].' '.$appoints['doctorLasttname']; ?></h1>
+                        <h1><b>reden: </b> <?php echo $appoints['reason'] ?></h1>
+                    </div>
+                <?php } ?>
+            </div>
+                
+        <?php } ?>
     </div>
-</div>
-<!--Hidden field for changing avatars-->
-<div id="avatarModal" class="modal"">
-<div class="modal-content">
-    <div class="container">
-        <h4>Kies Avatar</h4>
-        <?php foreach ($avatars as $row){?>
-            <div class="card">
-                <div class="col-md card">
-                    <img src="<?php echo $row['location'];?>" class="card-img-top dashboard-card-logo" alt="user image" height="50px" width="50px">
-                    <div class="card-body">
-                        <h5 class="card-title"style="text-align: center"><?php echo $row['title'];?></h5>
+
+    <!-- MODAL FOR AVATAR SELECTION -->
+    <div id="avatarModal" class="main-modal">
+        <div class="main-avatar-modal-content card main-card">
+            <h4>Kies Avatar</h4>
+            <div class="row main-avatar-modal-row">
+                <?php foreach ($avatars as $row) { ?>
+                    <div class="col-sm card main-avatar-modal-card">
+                        <img src="<?php echo $row['location'];?>" class="card-img-top main-avatar-modal-img" alt="user image">
                         <a onclick="submitAvatar('<?php echo $row['id'];?>','<?php echo $row['location'];?>')" class="stretched-link"></a>
                     </div>
-                </div>
+                    <div class="col-sm card main-avatar-modal-card-separator"></div>
+                <?php }?>
             </div>
+            <button type="submit" class="main-modal-btn" onclick="cancelAvatar()">cancel</button>
+        </div>
+    </div>
 
-        <?php } ?>
+</div>
+
+
+
 
 <script>
 function showCangePasword(){
