b62c7aa2379a45eddf1ab11dc2e71ff2d9371562
thibault3212 <thibault.vandenbroecke@student.kuleuven.be>
2021-05-11 14:00:32 +0200

    update

--- app/Controllers/DoctorsController.php
+++ app/Controllers/DoctorsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\doctorModel;
+
 class DoctorsController extends BaseController
 {
 
@@ -10,9 +12,19 @@ class DoctorsController extends BaseController
         if(session()->get('role')=='inhabitant') {
             return redirect()->to('/');
         } else {
-            echo view('templates/header');
+            $data=[];
+            $data["doctors"]=$this->getDoctors();
+            echo view('templates/header',$data);
             echo view('doctors');
             echo view('templates/footer');
         }
     }
+
+    private function getDoctors()
+    {
+        $doctorsModel= new doctorModel();
+        return $doctorsModel->select()->where('isActive',true)->get()->getResultArray();
+
+    }
+    public function doctorprofile($id){}
 }
